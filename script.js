8d2039bfaed7bfa3ac4108c73fece38a97409f82
Ponkoj Mondol <ponkojmonndal2014@gmail.com>
2022-12-11 20:40:44 +0600

    Project Upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// let rose = "red";
+// if (rose === "red") alert("Rose is a Beautiful flower...");
+
+let firstName = "Ponkoj";
+let lastName = "Mondol";
+
+console.log(firstName);
+console.log(firstName + lastName);
+console.log(firstName + lastName);
+console.log(firstName);
+console.log(firstName);
